4ec0968a27f094e40fcd10ddba055ab814de7a5a
ryo4004 <kato@ryo4004.net>
2022-04-22 01:46:33 +0900

    不要なルーティングを削除

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { RequestHandler } from 'express'
+import * as lib from './server/library'
+
 const app = express()
 
 app.use(express.json() as RequestHandler)
@@ -9,24 +11,6 @@ app.get('/', (req, res) => {
   console.log('[' + lib.showTime() + '] root access')
   res.redirect(301, 'https://zatsuzen.com')
 })
-
-const client = './client/build'
-app.use('/', express.static(client))
-app.use('/login', express.static(client))
-app.use('/signup', express.static(client))
-app.use('/payment', express.static(client))
-app.use('/list', express.static(client))
-app.use('/setting', express.static(client))
-app.use('/setting/osaifuname', express.static(client))
-app.use('/setting/rate', express.static(client))
-app.use('/setting/connect', express.static(client))
-app.use('/setting/disconnect', express.static(client))
-app.use('/setting/username', express.static(client))
-app.use('/setting/othername', express.static(client))
-app.use('/setting/password', express.static(client))
-app.use('/setting/userdelete', express.static(client))
-
-import * as lib from './server/library'
 
 // CORSを許可する
 app.use((req, res, next) => {
